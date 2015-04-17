1831cec8c862b600b1492c9883ff7c863b02fb05
corchodiaz <ddiaz@ing.unrc.edu.ar>
2015-04-17 18:33:09 -0300

    Nuevos archivos para el Procesamiento de Teclas

---
+++ inc/procesarTeclas.h
@@ -0,0 +1,65 @@
+
+/* IMPORTANTE: Este es un archivo para lectura de teclado utilizado
+ * para hacer pruebas rápidas, dado que no resuelve de manera eficiente
+ * el manejo de los rebotes.
+ */
+
+
+#ifndef PROCESARTECLAS_H
+#define PROCESARTECLAS_H
+/** \brief Short description of this file
+ **
+ ** Long description of this file
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Template Template to start a new module
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * yyyymmdd v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+uint8_t tiltLed;
+uint16_t tiltPer;
+uint8_t teclas;
+
+/*==================[external functions declaration]=========================*/
+
+// Procesamiento de Teclas por MODBUS
+extern void procesarTeclasModBus(uint8_t value);
+
+// Procesamiento de Teclas propio
+extern void procesarTeclas(void);
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+}
+#endif
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef TECLADO_H */
+
